a90ad0a7a46114a059b62634b918ca16b7d61d41
DHRUV CHAUHAN <163533922+DHRUVCHAUHAN6398@users.noreply.github.com>
2024-06-19 12:39:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Java Full-Stack Developer", "Web Developer", "Front-end Developer", "Java Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
